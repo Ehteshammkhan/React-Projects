8bd070ef9e179e404e9ce6db4183f1e13315962b
Ehteshammkhan <ehatishamkhan2001@gmail.com>
2024-08-13 15:11:05 +0530

    testing changes

--- reactRouter/src/components/Github/Github.jsx
+++ reactRouter/src/components/Github/Github.jsx
@@ -17,6 +17,7 @@ function Github() {
         <>
             <a className='block px-4 py-2 bg-orange-700 text-white rounded-md text-center' href={data.html_url} target='_blank'>Click here to visit the GitHub account</a>
             <img className='github-img' src={data.avatar_url} style={{ width: '200px', height: '200px', margin: '0 auto' }} alt="" />
+            <h1>Testing</h1>
         </>
     )
 }
